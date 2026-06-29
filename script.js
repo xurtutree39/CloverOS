d915e221d4db05e9be41fd9298361fce87cf4140
xurtutree <xurtutree@yahoo.com>
2026-06-30 00:23:29 +0100

    General Work
    Created a styles.css file
    Made windows open and close
    Fixed weird issue with opening a window after closing it

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ function dragElement(element) {
     document.onmousemove = null;
   }
 }
+var welcomeScreen = document.querySelector("#welcome")
+function closeWindow(element) {
+  element.style.display = "none"
+}
+function openWindow(element) {
+  element.style.display = "flex"
+}
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
+
